715975dcd21d3ace569fe40985c919e441b1574a
GangothriDJoy <118897141+GangothriDJoy@users.noreply.github.com>
2024-04-30 14:57:46 +0530

    Create childsum.c

---
+++ c_codes/system_calls/parentchildsum/childsum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+        int a, b, c;
+        printf("Enter the first number: ");
+        scanf(" %d", &a);
+        printf("Enter the second number: ");
+        scanf(" %d", &b);
+        c=a+b;
+        printf("The sum is : %d", c);
+        printf("\n\n");
+        return 0;
+}
+
+
